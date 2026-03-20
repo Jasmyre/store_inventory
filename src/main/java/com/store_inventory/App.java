cf51434d43484be1ff6b4d783a0b1539951962c9
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-03-20 09:34:00 +0800

    experimental(app): Add experimental app

---
+++ src/main/java/com/store_inventory/App.java
@@ -0,0 +1,5 @@
+package com.store_inventory;
+
+public class App {
+    
+}
---
+++ src/main/java/com/store_inventory/ai_protorype/Category.java
@@ -0,0 +1,9 @@
+package com.store_inventory.ai_protorype;
+
+public enum Category {
+    DAIRY,
+    BAKERY,
+    PRODUCE,
+    BEVERAGE,
+    OTHER
+}
